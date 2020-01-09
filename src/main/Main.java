9c6330f73bfb369fa0c2662b2a286f20c833c942
Jaziel Moreira <jaziel@copin.ufcg.edu.br>
2020-01-09 14:16:03 -0300

    Update Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,100 +1,24 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-		String repositoryUrl = null;
-		String repositoryList = null;
+		
+		/*
+		 * Set the output and download folder or comment these lines.
+		 * The default output folder is this directory.
+		 * The default download folder is the TEMP directory*/
+		FileUtils.setOutputFolder("outputFolder");
+		FileUtils.setDownloadFolder("downloadFolder");
+		
+		/*
+		 * Set the repository URL, the initial commit and whether it should create a log file. 
+		 */
+		String repositoryUrl = "Enter the repository URL here";
 		String initialCommit = "HEAD";
-		String outputFolder = null;
-		String downloadFolder = null;
 		boolean shouldLog = false;
-		
-		for(int i = 0; i<args.length; i++){
-			switch(args[i]) {
-			case "--url":
-			case "-u":
-				repositoryUrl = args[++i];
-				break;
-			case "--list":
-			case "-l":
-				repositoryList = args[++i];
-				break;
-			case "--init":
-			case "-i":
-				initialCommit = args[++i];
-				break;
-			case "--output":
-			case "-o":
-				outputFolder = args[++i];
-				break;
-			case "--download":
-			case "-d":
-				downloadFolder = args[++i];
-				break;
-			case "-log":
-				shouldLog = Boolean.parseBoolean(args[++i]);
-				break;
-			case "-help":
-				help();
-				break;
-			default:
-			}
-		}
-		
-		if(outputFolder!=null && !outputFolder.isEmpty()) {
-			FileUtils.setOutputFolder(outputFolder);
-		}
-		
-		if(downloadFolder!=null && !downloadFolder.isEmpty()) {
-			FileUtils.setDownloadFolder(downloadFolder);
-		}
-		
-		
-		if(repositoryList != null) {
-			List<String> urls = getUrls(repositoryList);
-			for(String url: urls) {
-				Finder finder = new Finder(url);
-				finder.findRefactorings("HEAD", shouldLog);
-			}
-			
-		}else if (repositoryUrl != null) {
-			Finder finder = new Finder(repositoryUrl);
-			finder.findRefactorings(initialCommit, shouldLog);
-			
-		}else
-			throw new Exception("No url provided!");
+
+		Finder finder = new Finder(repositoryUrl);
+		finder.findRefactorings(initialCommit, shouldLog);
 	}
-	
-	private static void help() {
-		System.out.println("-u, --url:         Repository's url *                      ");
-		System.out.println("-l, --list:        File with list of repository's url *    ");
-		System.out.println("-i, --init:        Initial commit                        (Default: \"HEAD\")");
-		System.out.println("-O, --output:      Output folder                         (Default: This directory)");
-		System.out.println("-d, --download:    Download folder                       (Default: Temp directory)");
-		System.out.println("-log:              Log errors                            (Default: False)");
-		System.out.println();
-		System.out.println(" * It is necessary a repository's urls or file with a list of repository's url");
-	}
-	
-	private static List<String> getUrls(String listFile) throws IOException{
-		ArrayList<String> urls = new ArrayList<>();
-		FileReader reader = new FileReader(listFile);
-		BufferedReader buff = new BufferedReader(reader);
-		
-		String url = buff.readLine();
-		while(url != null) {
-			urls.add(url);
-			url = buff.readLine();
-		}
-		buff.close();
-		return urls;
-	}
-	
 }
